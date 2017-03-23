31af05e3df50381d28165a3d3f0d05c9d2590c6f
Neeraj Surana <surananeeraj92@gmail.com>
2017-03-23 23:57:54 +0530

    deleted old files

--- public/assets/initializers/recaptcha-08c79d19d2c58d0edef8dcb15e7d0d29728c09e060135eae850bbf2f60ea2310.rb
+++
@@ -1,5 +0,0 @@
-Recaptcha.configure do |config|
-  config.site_key  = APP_CONFIG['recaptcha_site_key']
-  config.secret_key = APP_CONFIG['recaptcha_secret_key']
-  config.proxy = 'https://www.google.com/recaptcha/api/siteverify'
-end
