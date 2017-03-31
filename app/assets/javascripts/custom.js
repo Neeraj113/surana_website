6882f3c7f0663788c1b1d40eebf8da179d78e7b1
Neeraj Surana <surananeeraj92@gmail.com>
2017-03-31 10:26:43 +0530

    refactored code

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -51,16 +51,14 @@ $(function() {
                 $("#success > .alert-success").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
                 $("#success > .alert-success").append("<strong>Our Team will get back to you as soon as possible. </strong>");
                 $("#success > .alert-success").append("</div>");
-                $("#contactForm").trigger("reset");
-                grecaptcha.reset();
             } else {
                 $("#success").html("<div class='alert alert-danger'>");
                 $("#success > .alert-danger").html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
                 $("#success > .alert-danger").append("<strong>Sorry, the captcha is wrong. Please try again!");
                 $("#success > .alert-danger").append("</div>");
-                $("#contactForm").trigger("reset");
-                grecaptcha.reset();
             }
+            $("#contactForm").trigger("reset");
+            grecaptcha.reset();
         };
         var errorHandler = function(){
             $("#success").html("<div class='alert alert-danger'>");
