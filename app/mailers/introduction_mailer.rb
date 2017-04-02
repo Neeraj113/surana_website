8a32ad6aab4cff73f975efcc4730bc4b9f4e66c2
Neeraj Surana <surananeeraj92@gmail.com>
2017-04-02 12:48:16 +0530

    code changes

--- app/mailers/introduction_mailer.rb
+++ app/mailers/introduction_mailer.rb
@@ -1,12 +1,14 @@
 class IntroductionMailer < ApplicationMailer
 	default from: "suranasanitations@gmail.com"
 
+	# Send Mail To Customer, thanking him to contact us and we will get back to him soon.
 	def introduction_email(user)
 	    @user = user
 	    attachments['Introduction_Letter.pdf'] = File.read("#{Rails.root}/public/introduction_letter.pdf")
 	    mail(to: @user['email'], subject: 'Greetings From Surana Sanitations!')
 	end
 
+	# Send Mail To Owner, and giving him the details of the user who tried to contact him.
 	def contact_form(details)
 		@details = details
 		mail(to: 'suranagv@gmail.com', subject: 'Gaurav, A User Just Tried to contact you!')
